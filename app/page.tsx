0c62eb8fcc5f019b6616f7b048722d4311fdbe1c
5war00p <maddisaiswaroop@gmail.com>
2024-04-18 19:08:50 +0530

    feat: basic react flow test component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import TestDrag from "@/components/TestDrag";
 import TestDrop from "@/components/TestDrop";
+import TestReactFlow from "@/components/TestReactFlow";
 
 export default function Home() {
   return (
-    <>
-      <TestDrag />
-      <TestDrop />
-    </>
+    <div className="flex">
+      <TestReactFlow />
+
+      <div className="max-w-sm">
+        <TestDrag />
+        <TestDrop />
+      </div>
+    </div>
   );
 }
---
+++ components/TestReactFlow.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React, { useCallback } from "react";
+import ReactFlow, {
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Connection,
+  Controls,
+  MiniMap,
+} from "reactflow";
+
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export default function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params: Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  return (
+    <div className="h-screen w-11/12">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <Background />
+        <Controls />
+        <MiniMap />
+      </ReactFlow>
+    </div>
+  );
+}
